ef77a807a6f7821165b1ee5f730c472543fe2785
Rory Simmonds <rory@blankbox.business>
2024-03-03 01:22:24 +0000

    add container, navbar and row components

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import './style.css'
+
+
+function Navbar() {
+  return (
+    <nav>
+
+    </nav>
+  );
+}
+
+export default Navbar;

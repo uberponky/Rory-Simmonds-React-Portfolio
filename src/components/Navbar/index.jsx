5c66e7516c912d5fbf054d8d356404d40bf3e5dc
Rory Simmonds <rory@blankbox.business>
2024-03-04 21:07:43 +0000

    add navbar component

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,9 +5,31 @@ import './style.css'
 
 function Navbar() {
   return (
-    <nav>
-
+    <nav className="navbar fixed-top navbar-expand-sm">
+      <div className="container navbar-container pt-2 pb-3">
+        <a className="navbar-brand d-flex align-items-center" href="#">
+          <img 
+            src="./src/assets/BB.svg" 
+            alt="Blank Box Logo" 
+            width="30" 
+            height="30" 
+            className="d-inline-block align-text-top text-white pe-2"
+          ></img>
+          Rory Simmonds
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link" href="#portfolio">About</a>
+            <a className="nav-link" href="#skills">Projects</a>
+            <a className="nav-link" href="#contact">Contact</a>
+          </div>
+        </div>
+      </div>
     </nav>
+
   );
 }
 

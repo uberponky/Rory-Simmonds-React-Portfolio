4cbb97803e66eca2ac64b6a5fea4180f540f0725
Rory Simmonds <119239657+uberponky@users.noreply.github.com>
2024-10-08 10:13:05 +0100

    Update index.jsx

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -10,9 +10,9 @@ function Card({ title, description, img, deployedURL, githubURL, sitePreview }) 
           <h5 className="card-title">{ title }</h5>
           <p className="card-text">{ description }</p>
           <div className="d-flex flex-row gap-2 flex-wrap justify-content-center">
-            { deployedURL ? <a href={ deployedURL } className="btn project-btn">Deployed Site</a> : '' }
-            { sitePreview ? <a href={ sitePreview } className="btn project-btn">Video Preview</a> : '' }
-            { githubURL ? <a href={ githubURL } className="btn project-btn">GitHub Repo</a> : '' }
+            { deployedURL ? <a href={ deployedURL } target="_blank"> className="btn project-btn">Deployed Site</a> : '' }
+            { sitePreview ? <a href={ sitePreview } target="_blank"> className="btn project-btn">Video Preview</a> : '' }
+            { githubURL ? <a href={ githubURL } target="_blank"> className="btn project-btn">GitHub Repo</a> : '' }
           </div>
         </div>
       </div>
